c39ee9357eb9a40ebb154062687297e439f49c1c
Rocko <Rockokaine@gmail.com>
2021-09-22 14:41:40 -0700

    sleep coding dont remember

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const gameArea = document.getElementById('game-area');
-const rainbowWords = ["I", "Like / like", "The / the", "Look / look", "At / at", "My / my", "A / a", "See / see", "Me / me"];
+const rainbowWords = ["I", "Like", "The", "Look", "At", "My", "A", "See", "Me"];
+const playBtn = document.getElementById('play-button');
+const audio = document.querySelector('audio');
+const wordCards = document.querySelectorAll('.the-words');
 
 function populateGameArea() {
     for (let word of rainbowWords) {
-        gameArea.innerHTML += `<span class="the-words">${word}</span>`;
+        gameArea.innerHTML += `<span class="the-words" onClick="playCard(this.innerText)"><p>${word}</p></span>`;
     }
 
 }
 
+function playWord(e) {
+    audio.src = `audio/round1/${rainbowWords[Math.floor(Math.random() * rainbowWords.length + 1)].toLowerCase()}.ogg`;
+    console.log(e.target.innerText)
+    audio.play();
+}
+
+
+function playCard(e) {
+    audio.src = `audio/round1/${e.toLowerCase()}.ogg`;
+    audio.play();
+}
+
+
 populateGameArea();
+
+playBtn.addEventListener('click', playWord);
+
+
+
